1fee07e79b8f8480b087f4cf9ac512fae36011af
Mateus <Mateus@Mateus-PC>
2017-09-08 18:40:51 -0300

    INITIAL STAGE

--- src/main/java/br/com/events/bean/EventoBean.java
+++ src/main/java/br/com/events/bean/EventoBean.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileFilter;
 import java.io.IOException;
 import java.io.Serializable;
+import java.util.Arrays;
+import java.util.Comparator;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
@@ -50,9 +52,14 @@ public class EventoBean extends ImagemServlet implements Serializable {
 		try {
 			File folder = new File(pathEvento);
 			File[] listOfFolders = folder.listFiles(File::isDirectory);
-			
+
+			Arrays.sort(listOfFolders, new Comparator<File>() {
+				public int compare(File f1, File f2) {
+					return Long.valueOf(f2.lastModified()).compareTo(f1.lastModified());
+				}
+			});
 			eventos = listOfFolders;
-			
+
 		} catch (RuntimeException erro) {
 			Messages.addGlobalError("Ocorreu um erro ao tentar listar os diretórios");
 			erro.printStackTrace();
@@ -76,6 +83,11 @@ public class EventoBean extends ImagemServlet implements Serializable {
 				if (filter.isDirectory())
 					return filter.getName().toLowerCase().contains(getEventosFiltered().toLowerCase());
 				return false;
+			}
+		});
+		Arrays.sort(listOfFolders, new Comparator<File>() {
+			public int compare(File f1, File f2) {
+				return Long.valueOf(f2.lastModified()).compareTo(f1.lastModified());
 			}
 		});
 		eventos = listOfFolders;
